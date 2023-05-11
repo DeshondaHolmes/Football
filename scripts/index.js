55eadb2302ecc6e366fe1e1a36cc5057b4d9fffe
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-05-11 10:52:46 -0400

    Add html structure and scripts

--- scripts/index.js
+++ scripts/index.js
@@ -5,27 +5,27 @@ let teams = [
     { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
     { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
     { code: "HOU", name: "Houston Texans", plays: "Houston, TX" },
-    {
-        code: "KAN", name: "Kansas City Chiefs",
-        plays: "Kansas City, MO"
-    },
+    { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
 ];
 
 window.onload = function () {
     // load the dropdown list
-    let states = ["Alabama", "Alaska", "Arizona", ... ];
-    let abbrev = ["AL", "AK", "AZ", ... ];
-    const statesList = document.getElementById("statesList");
-    let length = states.length;
+    initTeamsDropdown();
+    // other stuff
+    console.log("hello");
+};
+function initTeamsDropdown() {
+    // load the dropdown list
+    let teams = ["Dallas Cowboys", "Denver Broncos", "Houston Texans", "Kansas City Chiefs" ];
+    let code = ["DAL", "DEN", "HOU", "KAN" ];
+    const teamsList = document.getElementById("teamsList");
+    let length = teams.length;
     for (let i = 0; i < length; i++) {
-        // create the option element
-        let theOption = document.createElement("option");
-        // set the text and value of the option you created
-        theOption.textContent = states[i];
-        theOption.value = abbrev[i];
+        // create the option element and set the text and
+        // value at the same time
+        let theOption = new Option(teams[i], code[i]);
         // append the option as a child of (inside) the 
         // select element
-        statesList.appendChild(theOption);
+        teamsList.appendChild(theOption);
     }
-
-};
+}
