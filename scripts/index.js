6e289bea76997bad4d0b199086faa3d90c86310b
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-05-11 11:34:07 -0400

    Fixed typo to allow html linking

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 let teams = [
     { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
     { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
@@ -8,24 +7,24 @@ let teams = [
     { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
 ];
 
+
+
 window.onload = function () {
     // load the dropdown list
-    initTeamsDropdown();
-    // other stuff
-    console.log("hello");
-};
-function initTeamsDropdown() {
-    // load the dropdown list
-    let teams = ["Dallas Cowboys", "Denver Broncos", "Houston Texans", "Kansas City Chiefs" ];
-    let code = ["DAL", "DEN", "HOU", "KAN" ];
+    let text = ["Dallas Cowboys", "Denver Broncos", "Houston Texans", "Kansas City Chiefs"];
+    let value = ["DAL", "DEN", "HOU", "KAN"];
     const teamsList = document.getElementById("teamsList");
-    let length = teams.length;
+    let length =text.length;
     for (let i = 0; i < length; i++) {
         // create the option element and set the text and
         // value at the same time
-        let theOption = new Option(teams[i], code[i]);
+        let theOption = new Option(teamNames[i], value[i]);
         // append the option as a child of (inside) the 
         // select element
         teamsList.appendChild(theOption);
+
+        console.log("Hello");
     }
-}
+   
+
+};
