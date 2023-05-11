1c755532d1c7956a3ca9dd25bf4a8c818be1576d
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-05-11 12:04:42 -0400

    add paragraph id

--- scripts/index.js
+++ scripts/index.js
@@ -14,17 +14,29 @@ window.onload = function () {
     let text = ["Dallas Cowboys", "Denver Broncos", "Houston Texans", "Kansas City Chiefs"];
     let value = ["DAL", "DEN", "HOU", "KAN"];
     const teamsList = document.getElementById("teamsList");
-    let length =text.length;
+    let length = text.length;
     for (let i = 0; i < length; i++) {
         // create the option element and set the text and
         // value at the same time
-        let theOption = new Option(teamNames[i], value[i]);
+        let theOption = new Option(text[i], value[i]);
         // append the option as a child of (inside) the 
         // select element
         teamsList.appendChild(theOption);
 
         console.log("Hello");
     }
-   
 
+    const button = document.getElementById("button");
+    button.onclick = onButtonClick;
+    console.log("click");
 };
+
+function onButtonClick() {
+    const teamsList = document.getElementById("teamsList");
+    const selectedTeamCode = teamsList.value;
+    const selectedTeam = teams.find(team => team.code === selectedTeamCode);
+    const teamPara = document.getElementById("teamPara");
+    console.log(selectedTeam);
+    teamPara.innerHTML = "Hi";
+
+}
