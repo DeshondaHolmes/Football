e9f146f36813305536ca5190ab0b2ff8d6c7379c
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-22 21:25:49 -0400

    Added final script for message

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const teamsList = document.getElementById("teamsList");
+
+const button = document.getElementById("button");
+
+const teamPara = document.getElementById("teamPara");
+
+
 let teams = [
     { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
     { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
@@ -11,14 +18,22 @@ let teams = [
 
 window.onload = function () {
     // load the dropdown list
+    button.onclick = onButtonClick;
+
+
     let text = ["Dallas Cowboys", "Denver Broncos", "Houston Texans", "Kansas City Chiefs"];
+
     let value = ["DAL", "DEN", "HOU", "KAN"];
-    const teamsList = document.getElementById("teamsList");
+
+
     let length = text.length;
     for (let i = 0; i < length; i++) {
         // create the option element and set the text and
         // value at the same time
-        let theOption = new Option(text[i], value[i]);
+        let theOption = document.createElement("option");
+        theOption.innerText = text[i];
+        theOption.value = value[i];
+        console.log(theOption);
         // append the option as a child of (inside) the 
         // select element
         teamsList.appendChild(theOption);
@@ -26,20 +41,29 @@ window.onload = function () {
         console.log("Hello");
     }
 
-    const button = document.getElementById("button");
-    button.onclick = onButtonClick;
-    console.log("click");
 };
 
+
+
+
+
 function onButtonClick() {
-    const teamsList = document.getElementById("teamsList");
-    const selectedTeamCode = teamsList.value;
-    const selectedTeam = teams.find(team => team.code === selectedTeamCode);
-    const teamPara = document.getElementById("teamPara");
-    console.log(selectedTeam.text);
-    let texasDescription = "The Dallas Cowboys are a professional American football team";
-    let texasmessage = texasDescription
-    teamPara.innerHTML = texasmessage;
-  
+
+
+    const selectedValue = teamsList.value;
+
+    console.log(selectedValue);
+
+    if (teamsList.selectedIndex >= 0) {
+        alert(`You selected a value`);
+    }
+
+    const selectedTeam = teams.find(team => team.code === selectedValue);
+
+    let message = `You selected the ${selectedTeam.name} (${selectedValue}) who play in ${selectedTeam.plays}.`;
+    console.log(selectedTeam.name);
+
+    teamPara.innerHTML = message;
+
 
 }
